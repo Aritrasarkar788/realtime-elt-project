71475b8092ce807b7308a9bb15d16e84738cf718
Aritra Sarkar <raritrastallonesarkar@gmail.com>
2025-07-22 15:23:11 +0530

    Update to use CoinGecko and GitHub secret for MongoDB

--- scripts/fetch_api_data.py
+++ scripts/fetch_api_data.py
@@ -1,15 +1,22 @@
+import os
 from pymongo import MongoClient
 import certifi
 import requests
 import datetime
+import socket
 
-uri = "mongodb+srv://Aritra:Aritra98@cluster0.qvndesi.mongodb.net/?retryWrites=true&w=majority"
+# Force IPv4 to avoid DNS issues on GitHub runners
+orig_getaddrinfo = socket.getaddrinfo
+def getaddrinfo_ipv4(*args, **kwargs):
+    return [ai for ai in orig_getaddrinfo(*args, **kwargs) if ai[0] == socket.AF_INET]
+socket.getaddrinfo = getaddrinfo_ipv4
+
+uri = os.environ["MONGO_URI"]
 client = MongoClient(uri, tlsCAFile=certifi.where())
 db = client["real_time_pipeline"]
 collection = db["crypto_data"]
 
-# Sample API
-url = url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
 response = requests.get(url)
 
 if response.status_code == 200:
